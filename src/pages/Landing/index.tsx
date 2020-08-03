8f6cfa79b7f49e40570db671ba4a4ad849ba72b5
Cassio Almeida <cassioaafilho@gmail.com>
2020-08-03 13:53:19 -0300

    assets and global.css

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div id="logo-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
